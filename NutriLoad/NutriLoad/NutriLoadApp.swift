d2d40ba22c300410f5a86426c18a0ffe6bc2a1c2
Atakan8479 <ozcan8479@gmail.com>
2026-05-30 01:05:21 +0300

    chore: initial project scaffolding

---
+++ NutriLoad/NutriLoad/NutriLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLoadApp.swift
+//  NutriLoad
+//
+//  Created by Atakan Özcan on 30.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct NutriLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutriLoad/NutriLoadTests/NutriLoadTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLoadTests.swift
+//  NutriLoadTests
+//
+//  Created by Atakan Özcan on 30.05.2026.
+//
+
+import Testing
+@testable import NutriLoad
+
+struct NutriLoadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
